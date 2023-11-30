4342186ac0f9cd2ad424c6864fce5fc5104de084
Amanda <amandawebstart@gmail.com>
2023-11-30 13:41:06 +0100

    add socket.io

--- app.js
+++ app.js
@@ -1,50 +1,39 @@
-const WebSocket = require('ws');
-const wss = new WebSocket.Server({ port: 3000 });
+const express = require('express');
+const http = require('http');
+const socketIO = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
+
+app.use(express.static(__dirname + '/'));
 
 let players = [];
-let playerCount = 0;
 
-wss.on('connection', (ws) => {
+io.on('connection', (socket) => {
   console.log('Nouvelle connexion');
 
-  // Attribuer un ID unique au joueur
-  const playerId = ++playerCount;
-  players.push({ id: playerId, socket: ws });
-
-  // Envoyer les informations des autres joueurs au nouveau joueur
-  players
-    .filter(player => player.id !== playerId)
-    .forEach(player => {
-      ws.send(JSON.stringify({ type: 'existingPlayer', id: player.id }));
-    });
-
-  // Gestion des messages
-  ws.on('message', (message) => {
-    const receivedData = JSON.parse(message);
-    switch (receivedData.type) {
-      case 'newPlayer':
-        // Gérer l'ajout d'un nouveau joueur (pas nécessaire ici)
-        break;
-      case 'paddle':
-        // Gérer les mouvements de la raquette pour le joueur 2
-        players.forEach((player) => {
-          if (player.socket !== ws) {
-            player.socket.send(message);
-          }
-        });
-        break;
-      // Ajouter d'autres cas au besoin
-      default:
-        break;
-    }
+  // Gérer l'ajout d'un nouveau joueur
+  socket.on('newPlayer', () => {
+    players.push(socket.id);
+    // Émettre un événement pour informer les clients qu'un nouveau joueur a rejoint
+    io.emit('playerJoined', players);
   });
 
-  // Gestion de la déconnexion
-  ws.on('close', () => {
-    console.log('Connexion fermée');
-    // Supprimer le joueur déconnecté de la liste
-    players = players.filter((player) => player.socket !== ws);
+  // Gérer les mouvements de la raquette
+  socket.on('paddleMovement', (y) => {
+    // Émettre un événement pour mettre à jour les mouvements des raquettes
+    socket.broadcast.emit('opponentPaddle', { id: socket.id, y });
+  });
+
+  // Gérer la déconnexion d'un joueur
+  socket.on('disconnect', () => {
+    players = players.filter(playerId => playerId !== socket.id);
+    // Émettre un événement pour informer les clients qu'un joueur s'est déconnecté
+    io.emit('playerDisconnected', players);
   });
 });
 
-console.log('Serveur WebSocket en écoute sur le port 3000');
+server.listen(3000, () => {
+  console.log('Serveur en écoute sur le port 3000');
+});
